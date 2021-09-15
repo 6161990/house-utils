6db6cccf5b5fcff1a89240f342d9268c236b9cb0
6161990 <6161990src@gmail.com>
2021-09-15 20:44:25 +0900

    Setting House Utils Env

---
+++ src/main/java/com/yoonji/houseutils/HouseutilsApplication.java
@@ -0,0 +1,13 @@
+package com.yoonji.houseutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseutilsApplication.class, args);
+    }
+
+}
