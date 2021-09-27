2b6b5db32fbeb886fbbacab86957199a99e50208
6161990 <6161990src@gmail.com>
2021-09-27 20:57:00 +0900

    BrokeragePolicyFactory 예외 처리 수정

--- src/main/java/com/yoonji/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/yoonji/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.yoonji.houseutils.policy;
 
 import com.yoonji.houseutils.constants.ActionType;
+import com.yoonji.houseutils.exception.ErrorCode;
+import com.yoonji.houseutils.exception.HouseUtilsException;
 
 public class BrokeragePolicyFactory {
 
@@ -11,7 +13,8 @@ public class BrokeragePolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default:
-                throw new IllegalArgumentException("해당 action type에 대한 정책이 존재하지 않습ㄴ디ㅏ.");
+            //    throw new IllegalArgumentException("해당 action type에 대한 정책이 존재하지 않습ㄴ디ㅏ.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST,"해당 actionType에 대한 정책이 존재하지 않습니다.");
         }
     }
 }
