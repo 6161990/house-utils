53e3aa32d499980922120ae1e11715c3c89e24d6
6161990 <6161990src@gmail.com>
2023-01-04 16:05:46 +0900

    if 문으로 policy 생성하여 중개 수수료 계산하기

--- src/main/java/com/yoonji/houseUtils/controller/BrokerageQueryController.java
+++ src/main/java/com/yoonji/houseUtils/controller/BrokerageQueryController.java
@@ -1,6 +1,8 @@
 package com.yoonji.houseUtils.controller;
 
 import com.yoonji.houseUtils.constants.ActionType;
+import com.yoonji.houseUtils.policy.PurchaseBrokeragePolicy;
+import com.yoonji.houseUtils.policy.RentBrokeragePolicy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,14 @@ public class BrokerageQueryController {
     public Long calcPrice(@RequestParam ActionType actionType,
                           @RequestParam Long price){
         // 중개 수수료를 계산하는 로직
+        if(actionType == ActionType.PURCHASE){
+            PurchaseBrokeragePolicy policy = new PurchaseBrokeragePolicy();
+            return policy.calculate(price);
+        }
+        if(actionType == ActionType.RENT){
+            RentBrokeragePolicy policy = new RentBrokeragePolicy();
+            return policy.calculate(price);
+        }
         return null;
     }
 }
