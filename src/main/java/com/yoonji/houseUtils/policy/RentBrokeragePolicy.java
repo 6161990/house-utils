46e485a0cb0e6ed34eb67d4985db5f9c74753eac
6161990 <6161990src@gmail.com>
2023-01-04 15:59:16 +0900

    임대일 때, 중계 수수료를 계산하는 RentBrokeragePolicy

---
+++ src/main/java/com/yoonji/houseUtils/policy/RentBrokeragePolicy.java
@@ -0,0 +1,26 @@
+package com.yoonji.houseUtils.policy;
+/**
+ * 임대일 때, 중계 수수료를 계산한다.
+ * */
+public class RentBrokeragePolicy {
+    public Long calculate(Long price){
+        BrokerageRule rule = createBrokerageRule(price);
+        return rule.calcMaxBrokerage(price);
+    }
+
+    public BrokerageRule createBrokerageRule(Long price) {
+        BrokerageRule rule;
+        if(price < 50_000_000){
+            rule = new BrokerageRule(0.5, 250_000L);
+        }else if(price < 100_000_000){
+            rule = new BrokerageRule(0.4, 300_000L);
+        }else if(price < 300_000_000){
+            rule = new BrokerageRule(0.3, null); //상한 금액은 없음
+        }else if(price < 600_000_000){
+            rule = new BrokerageRule(0.4, null); //상한 금액은 없음
+        }else {
+            rule = new BrokerageRule(0.8, null); //상한 금액은 없음
+        }
+        return rule;
+    }
+}
