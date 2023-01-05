b23815d236d395fdeacb132bf7a8cda33f0251ef
6161990 <6161990src@gmail.com>
2023-01-05 14:06:24 +0900

    add test

--- src/test/java/com/yoonji/houseUtils/policy/BrokeragePolicyTest.java
+++ src/test/java/com/yoonji/houseUtils/policy/BrokeragePolicyTest.java
@@ -1,9 +1,10 @@
 package com.yoonji.houseUtils.policy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BrokeragePolicyTest {
 
@@ -18,6 +19,30 @@ class BrokeragePolicyTest {
 
     @Test
     void purchaseBrokeragePolicy_test() {
-        assertThat(purchaseBrokeragePolicy.calculate(30_000_000L)).isEqualTo(180_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(30_000_000L), 180_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(100_000_000L), 500_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(500_000_000L), 2_000_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(800_000_000L), 4_000_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(1_000_000_000L), 9_000_000L);
     }
+
+    @Test
+    void rentBrokeragePolicy_test() {
+        assertEquals(
+                rentBrokeragePolicy.calculate(30_000_000L), 150_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(100_000_000L), 300_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(500_000_000L), 2_000_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(800_000_000L), 6_400_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(1_000_000_000L), 8_000_000L);
+    }
+
 }
